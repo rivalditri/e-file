32e668973da80a42123f1347db9e15d325a5f61e
rivalditri <rivaldi07tri@gmail.com>
2023-07-21 09:02:00 +0700

    fix auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -25,8 +25,7 @@ class Auth extends CI_Controller
     {
         $data['title'] = 'E-file Perumda Air Minum Tugu Tirta';
         $nip = $this->input->post('nip');
-        $pwd = $this->input->post('password');
-        // $password = md5('tugutirta'.$pwd);
+        $pwd = md5($this->input->post('password'));
         $user = $this->db->get_where('user', ['nip' => $nip, 'password' => $pwd])->row_array();
         if ($user) {
             $this->load->view('dashboard/menu', $data);
@@ -38,24 +37,28 @@ class Auth extends CI_Controller
 
     public function registration()
     {
-        $this->load->view('auth/login_view');
-        // $data['title'] = 'Registration Page E-file Perumda Air Minum Tugu Tirta';
-        // $this->form_validation->set_rules('nip', 'NIP', 'required|min_length[8]|max_length[8]|trim');
-        // $this->form_validation->set_rules('password1', 'Password', 'required|min_length[4]|trim');
+        $this->form_validation->set_rules('nip', 'NIP', 'required|min_length[8]|max_length[8]|trim|is_unique[user.nip]', [
+            'is_unique' => 'NIP ini sudah terdaftar!'
+        ]);
+        // $this->form_validation->set_rules('nama', 'Nama', 'required|trim');
+        // $this->form_validation->set_rules('password1', 'Password', 'required|min_length[4]|trim|matches[password2]', [
+        //     'matches' => 'Password tidak sama!',
+        //     'min_length' => 'Password terlalu pendek!'
+        // ]);
         // $this->form_validation->set_rules('password2', 'Password', 'required|min_length[4]|trim|matches[password1]');
         // if ($this->form_validation->run() == false) {
-
+        //     $this->load->view('auth/register_view');
         // } else {
-        //     $data = [
-        //         'nip' => htmlspecialchars($this->input->post('nip', true)),
-        //         'password' => password_hash($this->input->post('password1'), md5('tugutirta')),
-        //         'role_id' => 2,
-        //         'is_active' => 1,
-        //         'date_created' => time()
-        //     ];
-        //     $this->db->insert('user', $data);
-        //     $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Selamat! Akun anda telah dibuat. Silahkan Login</div>');
-        //     redirect('auth');
+        // $this->_addUser();
         // }
+        $data = [
+            'nip' => htmlspecialchars($this->input->post('nip', true)),
+            'nama' => htmlspecialchars($this->input->post('nama', true)),
+            'role_id' => $this->input->post('role', true),
+            'password' => md5($this->input->post('password1')),
+        ];
+        $this->db->insert('user', $data);
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Selamat! Akun anda telah dibuat. Silahkan Login</div>');
+        redirect('auth');
     }
 }
